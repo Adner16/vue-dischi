eff315067fa9e0eb3360ecf8bdcaeaf1c56c4829
Adner16 <giovanni.renda16@gmail.com>
2022-02-13 23:34:58 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <header>
+      <img src="" alt="">
+  </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style>
+    header {
+        height: 60px;
+        background-color: darkblue;
+    }
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,44 @@
+<template>
+    <main>
+        <div class="container">
+            <ul>
+                <li v-for="(disc, index) in discs" :key='index'>{{ disc.title }}</li>
+            </ul>
+        </div>
+    </main>
+   
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data() {
+      return {
+        discs: []
+      }
+    },
+    created() {
+        this.getDiscs();
+    },
+    methods: {
+        async getDiscs(){
+            const res = await fetch('https://flynn.boolean.careers/exercises/api/array/music');
+            const {response} = await res.json();
+            this.discs=response;
+        }
+    },  
+}
+
+</script>
+
+<style scoped lang='scss'>
+    main{
+        background-color: lightblue;
+        height: 100vh;
+        .container{
+            width: 1200px;
+            margin: auto;
+        }
+
+    }
+</style>
